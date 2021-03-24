6b7a54ed27ac804a7a47ed1b778fa28f73f2dda6
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-03-24 18:42:18 +0000

    mathematic calculation usage example is added

--- main.cs
+++ main.cs
@@ -1,7 +1,35 @@
 using System;
+using System.Threading;
+using System.Collections.Generic;
 
 class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+    public static void Main (string[] args)
+    {
+        int count = 0;
+        foreach (var result in GetCalculations(100))
+        {
+            count++;
+            Console.WriteLine($"Result of {count} --> {result}");
+        }
+    }
+
+    public static IEnumerable<int> GetCalculations(int size)
+    {
+        var random = new Random();
+
+        var a = new List<int>(size);
+        var b = new List<int>(size);
+
+        for (int i = 0; i < size; i++)
+        {
+            a.Add(random.Next(100, 1000));
+            b.Add(random.Next(100, 1000));
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            Thread.Sleep(50);
+            yield return a[i] * b[i];
+        }
+    }  
 }
